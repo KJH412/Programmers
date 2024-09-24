4d1c3fd7a7783208c81cc0d1c8169e7a78832195
KIMJUNGHYUN <108113992+KJH412@users.noreply.github.com>
2024-09-24 13:50:14 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,14 @@
+function solution(numer1, denom1, numer2, denom2) {
+    //var answer = [];
+    
+    let numer = numer1 * denom2 + numer2 * denom1; 
+    let denom = denom1 * denom2;  
+    let gcd = cal_gcd(numer, denom);
+   
+    return [numer / gcd, denom/gcd]
+}
+
+// 최대 공약수 구하기
+function cal_gcd(a, b) {
+    return a % b === 0 ? b : cal_gcd(b, a % b)
+}
