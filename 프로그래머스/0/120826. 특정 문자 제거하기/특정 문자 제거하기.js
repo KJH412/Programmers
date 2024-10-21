260dd9f61fc6e21447fccd8263d914d86050894a
KIMJUNGHYUN <108113992+KJH412@users.noreply.github.com>
2024-10-22 02:27:13 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,9 +1,12 @@
 function solution(my_string, letter) {
     var answer = '';
     
-    //replace(substr, newSubstr); substr:제거할문자열, newSubstr:대체할문자열
+    //replace(substr, newSubstr); substr:제거 할 문자열, newSubstr:대체 할 문자열
     
-    //replace 메서드에서 변수를 정규표현식으로 사용 => RegExp 객체를 활용
+    //my_string.replace(/문자/g, '') => 정규표현식 '문자' 부분에 변수를 넣고 싶음
+    
+    //replace 메서드에서 변수를 정규표현식으로 사용 
+    //=> RegExp 객체를 활용, g 플래그는 전역 검색
     answer =  my_string.replace(new RegExp(letter, 'g'), '') 
    
     
