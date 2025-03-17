f411cf68d0766ca8c4263d5a2d59006f2f0b9ed6
KIM JUNG HYUN <108113992+KJH412@users.noreply.github.com>
2025-03-17 18:07:57 +0900

    [level 1] Title: 신고 결과 받기, Time: 3865.79 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,44 @@
+function solution(id_list, report, k) {// 유저id, 신고한 유저id, 정지 기준 횟수  4:18~
+    //var answer = [];
+    const newReport = [...new Set(report)]; //중복 제거
+    const result = new Array(id_list.length).fill(0); //받은 메일 개수
+    const count = new Array(id_list.length).fill(0);; //신고 횟수
+    const stopId = []; //정지된 id
+    
+    for(let i of newReport){
+        let userid = i.split(" ")[0];
+        let decUserid = i.split(" ")[1]; //신고한 ID
+        let idx = id_list.indexOf(decUserid); //신고받은 id 인덱스
+        
+        count[idx] += 1;
+        
+        if(count[idx] >= k){ //정지
+            stopId.push(id_list[idx]);
+        }
+    }
+    
+    newReport.map((arr, index) => {
+        let userid = arr.split(" ")[0];
+        let decUserid = arr.split(" ")[1];
+        
+        if(stopId.indexOf(decUserid) >= 0 ){
+            let index = id_list.indexOf(userid);
+            result[index] += 1;
+        }
+    })
+    
+    return result;
+}
+/*
+
+let map = new Map();
+report.forEach(arr => {
+    const [key, value]= arr.split(" ");
+    if(map.has(key)){
+        map.get(key).push(value);
+    }else{
+        map.set(key, [value]);
+    }
+});
+const reportMapObj = Object.fromEntries(map); //{"muzi":["frodo","neo"]}
+*/
